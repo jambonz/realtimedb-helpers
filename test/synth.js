d4ebc5b7f323ca52a7352514be5fa02189dbe431
Dave Horton <daveh@beachdognet.com>
2021-02-08 15:26:45 -0500

    modified synthAudio to return indicator of whether file was served from cache

--- test/synth.js
+++ test/synth.js
@@ -1,6 +1,7 @@
 const test = require('tape').test ;
 const config = require('config');
 const opts = config.get('redis');
+const fs = require('fs');
 
 process.on('unhandledRejection', (reason, p) => {
   console.log('Unhandled Rejection at: Promise', p, 'reason:', reason);
@@ -10,48 +11,71 @@ test('speech synth tests', async(t) => {
   const fn = require('..');
   const {synthAudio, client} = fn(opts);
 
-  if (!process.env.GOOGLE_APPLICATION_CREDENTIALS ||
-    !process.env.AWS_ACCESS_KEY_ID ||
-    !process.env.AWS_SECRET_ACCESS_KEY ||
-    !process.env.AWS_REGION) {
+  if (!process.env.GCP_FILE ||
+    !process.env.ACCESS_KEY_ID ||
+    !process.env.SECRET_ACCESS_KEY ||
+    !process.env.REGION) {
       t.pass('skipping speech synth tests since no credentials provided');
       t.end();
       client.quit();
       return;
   }
   try {
-    let filepath = await synthAudio({
+    const creds = JSON.parse(fs.readFileSync(process.env.GCP_FILE));
+    let opts = await synthAudio({
       vendor: 'google',
+      credentials: {
+        credentials: {
+          client_email: creds.client_email,
+          private_key: creds.private_key
+        }
+      },
       language: 'en-GB',
       gender: 'MALE', 
       text: 'This is a test.  This is only a test',
       salt: 'foo.bar'
     });
-    t.pass(`successfully synthesized non-cached google audio to ${filepath}`);
+    t.ok(!opts.servedFromCache, `successfully synthesized non-cached google audio to ${opts.filepath}`);
   
-    filepath = await synthAudio({
+    opts = await synthAudio({
       vendor: 'google',
+      credentials: {
+        credentials: {
+          client_email: creds.client_email,
+          private_key: creds.private_key
+        }
+      },
       language: 'en-GB',
       gender: 'MALE', 
       text: 'This is a test.  This is only a test'
     });
-    t.pass(`successfully synthesized cached google audio to ${filepath}`);
+    t.ok(opts.servedFromCache, `successfully synthesized cached google audio to ${opts.filepath}`);
 
-    filepath = await synthAudio({
+    opts = await synthAudio({
       vendor: 'aws',
+      credentials: {
+        accessKeyId: process.env.ACCESS_KEY_ID,
+        secretAccessKey: process.env.SECRET_ACCESS_KEY,
+        region: process.env.REGION
+      },
       language: 'en-US',
       voice: 'Amy', 
       text: 'This is a test.  This is only a test'
     });
-    t.pass(`successfully synthesized non-cached aws audio to ${filepath}`);
+    t.ok(!opts.servedFromCache, `successfully synthesized non-cached aws audio to ${opts.filepath}`);
 
-    filepath = await synthAudio({
+    opts = await synthAudio({
       vendor: 'aws',
+      credentials: {
+        accessKeyId: process.env.ACCESS_KEY_ID,
+        secretAccessKey: process.env.SECRET_ACCESS_KEY,
+        region: process.env.REGION
+      },
       language: 'en-US',
       voice: 'Amy', 
       text: 'This is a test.  This is only a test'
     });
-    t.pass(`successfully synthesized cached aws audio to ${filepath}`);
+    t.ok(opts.servedFromCache, `successfully synthesized cached aws audio to ${opts.filepath}`);
 
     await client.flushallAsync();
 
