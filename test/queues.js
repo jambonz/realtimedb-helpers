554ea487a785e5d25d6e8a4d748b5ea3f0bc92be
Dave Horton <daveh@beachdognet.com>
2021-06-17 08:35:51 -0400

    listQueues returns number of members in each queue

--- test/queues.js
+++ test/queues.js
@@ -14,30 +14,27 @@ process.on('unhandledRejection', (reason, p) => {
 
 test('queues tests', async(t) => {
   const fn = require('..');
-  const {listQueues, addKey, client} = fn(opts);
+  const {listQueues, pushBack, client} = fn(opts);
 
   //wait 1 sec for connection
   //await sleep(1); 
 
   try {
 
-    await addKey('queue:account-sid:queue1', 'another value');
-    await addKey('queue:account-sid:queue2', 'another value');
-    await addKey('queue:account-sid:queue3', 'another value');
-    await addKey('queue:account-sid:queue4', 'another value');
-    await addKey('queue:account-sid:queue5', 'another value');
-    await addKey('queue:account-sid:training-queue', 'another value');
+    await pushBack('queue:account-sid:queue1', 'url1');
+    await pushBack('queue:account-sid:queue1', 'url2');
+    await pushBack('queue:account-sid:queue1', 'url3');
+    await pushBack('queue:account-sid:queue1', 'url4');
+    await pushBack('queue:account-sid:training-queue', 'a value');
+    await pushBack('queue:account-sid:training-queue', 'another value');
 
     let queues = await listQueues('account-sid');
-    //console.log(queues);
-    t.ok(queues.length === 6, 'retrieved 6 total queues');
+    t.ok(queues.length === 2, 'retrieved 2 total queues');
 
     queues = await listQueues('account-sid', 'training-*');
-    //console.log(queues);
-    t.ok(queues.length === 1, 'retrieved 1 total queues by pattern');
+    t.ok(queues.length === 1 && queues[0].length === 2, 'retrieved 1 total queues by pattern');
 
     t.end();
-
   }
   catch (err) {
     console.error(err);
